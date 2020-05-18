bcfe288c2bae5271e1716f135548bdca26a5ad1d
Uema Yohei <winuim@gmail.com>
2020-05-18 10:33:05 +0900

    eライブラリのリンク追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,19 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+        {
+          resolve: "gatsby-remark-external-links",
+          options: {
+            target: "_self",
+            rel: "nofollow"
+          }
+        }
+        ]
+      }
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,9 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <a href="https://ela.kodomo.ne.jp/students">
+      <p>eライブラリ</p>
+    </a>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
