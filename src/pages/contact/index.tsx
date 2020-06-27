ba8fccf7184e26cbab7dbc297fadd6b8fec4d7ae
Uema Yohei <winuim@gmail.com>
2020-06-27 21:36:09 +0900

    forms sampleにformik追加

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { navigate } from "gatsby"
+import { useFormik } from "formik"
 
 import Layout from "../../components/layout"
 
@@ -21,13 +22,11 @@ export default function Contact() {
     email: "",
     message: "",
   })
-
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     setState({ ...state, [e.target.name]: e.target.value })
   }
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const form = e.currentTarget
@@ -42,6 +41,26 @@ export default function Contact() {
       .then(() => navigate(form.getAttribute("action") ?? ""))
       .catch(error => alert(error))
   }
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    onSubmit: (values, actions) => {
+      alert(JSON.stringify(values, null, 2))
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          "form-name": "contact",
+          ...values,
+        }),
+      })
+        .then(() => navigate("/contact/thanks/"))
+        .catch(error => alert(error))
+    },
+  })
 
   return (
     <Layout>
@@ -52,35 +71,51 @@ export default function Contact() {
         action="/contact/thanks/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
+        onSubmit={formik.handleSubmit}
       >
         {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
         <input type="hidden" name="form-name" value="contact" />
         <p hidden>
           <label>
-            Don’t fill this out:{" "}
-            <input name="bot-field" onChange={handleChange} />
+            Don’t fill this out: <input name="bot-field" />
           </label>
         </p>
         <p>
           <label>
             Your name:
             <br />
-            <input type="text" name="name" onChange={handleChange} />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
           </label>
         </p>
         <p>
           <label>
             Your email:
             <br />
-            <input type="email" name="email" onChange={handleChange} />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
           </label>
         </p>
         <p>
           <label>
             Message:
             <br />
-            <textarea name="message" onChange={handleChange} />
+            <textarea
+              id="message"
+              name="message"
+              onChange={formik.handleChange}
+              value={formik.values.message}
+            />
           </label>
         </p>
         <p>
