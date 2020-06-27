2ebcc7a411264d39876ba3db5725330efc7a73eb
Uema Yohei <winuim@gmail.com>
2020-06-27 22:09:09 +0900

    Formik使うように変更

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
+import React from "react"
 import { navigate } from "gatsby"
-import { useFormik } from "formik"
+import { Form, Formik, FormikHelpers } from "formik"
 
 import Layout from "../../components/layout"
 
@@ -17,111 +17,89 @@ interface States {
 }
 
 export default function Contact() {
-  const [state, setState] = useState<States>({
+  const initStates: States = {
     name: "",
     email: "",
     message: "",
-  })
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setState({ ...state, [e.target.name]: e.target.value })
   }
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const form = e.currentTarget
+  const handleSubmit = (values: States, actions: FormikHelpers<States>) => {
+    alert(JSON.stringify(values, null, 2))
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        "form-name": form.getAttribute("name"),
-        ...state,
+        "form-name": "contact",
+        ...values,
       }),
     })
-      .then(() => navigate(form.getAttribute("action") ?? ""))
+      .then(() => navigate("/contact/thanks/"))
       .catch(error => alert(error))
   }
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      message: "",
-    },
-    onSubmit: (values, actions) => {
-      alert(JSON.stringify(values, null, 2))
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({
-          "form-name": "contact",
-          ...values,
-        }),
-      })
-        .then(() => navigate("/contact/thanks/"))
-        .catch(error => alert(error))
-    },
-  })
 
   return (
     <Layout>
       <h1>Contact</h1>
-      <form
-        name="contact"
-        method="post"
-        action="/contact/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={formik.handleSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input
-              type="text"
-              id="name"
-              name="name"
-              onChange={formik.handleChange}
-              value={formik.values.name}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea
-              id="message"
-              name="message"
-              onChange={formik.handleChange}
-              value={formik.values.message}
-            />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <Formik initialValues={initStates} onSubmit={handleSubmit}>
+        {formik => (
+          <Form
+            name="contact"
+            method="post"
+            action="/contact/thanks/"
+            data-netlify={true}
+            data-netlify-honeypot="bot-field"
+            onSubmit={formik.handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out: <input name="bot-field" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your name:
+                <br />
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your email:
+                <br />
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message:
+                <br />
+                <textarea
+                  id="message"
+                  name="message"
+                  onChange={formik.handleChange}
+                  value={formik.values.message}
+                />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </Form>
+        )}
+      </Formik>
     </Layout>
   )
 }
